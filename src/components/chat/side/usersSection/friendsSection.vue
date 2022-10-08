dabebe1c1a2c07e385c598721e0a0311f8b1996b
Felipe Tavares <felipelimatavares96@gmail.com>
2022-10-08 08:04:13 -0300

    remove some userCards

--- src/components/chat/side/usersSection/friendsSection.vue
+++ src/components/chat/side/usersSection/friendsSection.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="friendsSectionContainer">
-    <UserCard userName="Felipe" userStatus="Estudando Front-end" MTime="00:00" MUnread="4"/>
-    <UserCard userName="Felipe" userStatus="Estudando Front-end" MTime="00:00" MUnread="4"/>
-    <UserCard userName="Felipe" userStatus="Estudando Front-end" MTime="00:00" MUnread="4"/>
-    <UserCard userName="Felipe" userStatus="Estudando Front-end" MTime="00:00" MUnread="4"/>
-    <UserCard userName="Felipe" userStatus="Estudando Front-end" MTime="00:00" MUnread="4"/>
-    <UserCard userName="Felipe" userStatus="Estudando Front-end" MTime="00:00" MUnread="4"/>
-    <UserCard userName="Felipe" userStatus="Estudando Front-end" MTime="00:00" MUnread="4"/>
-    <UserCard userName="Felipe" userStatus="Estudando Front-end" MTime="00:00" MUnread="4"/>
+    <UserCard
+      userName="Felipe"
+      userStatus="Estudando Front-end"
+      MTime="00:00"
+      MUnread="4"
+    />
+    <UserCard
+      userName="Jirlan"
+      userStatus="Estudando Front-end"
+      MTime="00:00"
+      MUnread="4"
+    />
   </div>
 </template>
 
@@ -17,31 +21,31 @@ import { defineComponent } from "vue";
 import UserCard from "../userCard/userCard.vue";
 
 export default defineComponent({
-    name: "friendsSection",
-    components: { UserCard }
-})
+  name: "friendsSection",
+  components: { UserCard },
+});
 </script>
 
 <style scoped lang="scss">
-  .friendsSectionContainer{
-    height: calc(100vh - 110px);
-    background-color: $c-lightblue;
-    border-radius: 14px 14px 0px 0px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    overflow-y: scroll;
-    padding-top: 20px;
-  }
+.friendsSectionContainer {
+  height: calc(100vh - 110px);
+  background-color: $c-lightblue;
+  border-radius: 14px 14px 0px 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: scroll;
+  padding-top: 20px;
+}
 
-  ::-webkit-scrollbar {
-    width: 10px;
-  }
-  ::-webkit-scrollbar-track {
-    background: $c-lightblue; 
-  }
-  ::-webkit-scrollbar-thumb {
-    background: $c-input-gray;
-    border-radius: 10px; 
-  }
+::-webkit-scrollbar {
+  width: 10px;
+}
+::-webkit-scrollbar-track {
+  background: $c-lightblue;
+}
+::-webkit-scrollbar-thumb {
+  background: $c-input-gray;
+  border-radius: 10px;
+}
 </style>
