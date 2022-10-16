03580b11bdbbfeeb9076cbdfcd7ec9bd43411747
Felipe Tavares <felipelimatavares96@gmail.com>
2022-10-15 21:33:09 -0300

    Add function to put the messages list

--- src/components/chat/SideBar/RoomCard/RoomCard.vue
+++ src/components/chat/SideBar/RoomCard/RoomCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="roomCard">
+  <div class="roomCard" @click="selectRoom()">
     <img class="roomPic" :src="roomAvatar" alt="roomName" />
 
     <div class="cardInfo">
@@ -10,14 +10,24 @@
 </template>
 
 <script lang="ts">
-export default {
+import { defineComponent } from "vue";
+import { mapActions } from "vuex";
+
+export default defineComponent({
   name: "RoomCard",
   props: {
+    roomId: String, /////////Lembrar disso
     roomName: String,
     roomAvatar: String,
     roomLastMsgTime: String,
   },
-};
+  methods: {
+    ...mapActions(["CHANGE_ACTUAL_ROOM"]),
+    selectRoom() {
+      this.CHANGE_ACTUAL_ROOM(this.roomId);
+    },
+  },
+});
 </script>
 
 <style scoped lang="scss">
