ddf16b7fcfa79556eab79920251bf0f883121f6e
Felipe Tavares <felipelimatavares96@gmail.com>
2022-10-17 14:56:58 -0300

    add member service

---
+++ src/services/api/addMember.ts
@@ -0,0 +1,18 @@
+import {  NewMemberData } from "@/types";
+import { HttpClient } from "../HttpClient";
+
+const httpClient = HttpClient.getInstance()
+
+
+export async function addMember(newMember:NewMemberData): Promise<boolean> {
+  try {
+    const response = await httpClient.client.post('/chat/rooms/users', newMember)
+    if (response.status === 204) {
+      return true
+    }
+    
+    return false
+  } catch {
+    return false
+  }
+}      
