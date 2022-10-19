6e4952d68c5c7792e20460f54ecb1996190700db
Felipe Tavares <felipelimatavares96@gmail.com>
2022-10-19 19:12:58 -0300

    Fix logic of redirection if not loggedIn

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,31 +2,19 @@ import { getToken } from "@/services/localStorage/AuthStorage";
 import store from "@/store";
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
+import Auth from "../views/AuthenticationView.vue";
+import Chat from "../views/ChatView.vue";
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Auth",
-    component: () => import("../views/AuthenticationView.vue"),
-    async beforeEnter(_, __, next) {
-      if (!store.state.userInfo.isLoggedIn) {
-        const token = getToken();
-        console.log(token);
-        if (token) {
-          await store.dispatch("SET_TOKEN", token);
-          next({
-            name: "Chat",
-          });
-        }
-        next({
-          name: "Auth",
-        });
-      }
-    },
+    component: Auth,
   },
   {
     path: "/chat",
     name: "Chat",
-    component: () => import("../views/ChatView.vue"),
+    component: Chat,
   },
 ];
 
@@ -35,22 +23,23 @@ const router = createRouter({
   routes,
 });
 
-/* router.beforeEach(async (to, from, next) => {
-  console.log("Estou aqui:", store.state.userInfo);
-  console.log(to.name, from.name);
-  if (!store.state.userInfo.isLoggedIn) {
+router.beforeEach(async (to, from, next) => {
+  if (to.name !== "Auth" && !store.state.userInfo.isLoggedIn) {
+    console.log("está em chat");
     const token = getToken();
     console.log(token);
     if (token) {
       await store.dispatch("SET_TOKEN", token);
+      next();
+    } else {
       next({
-        name: "Chat",
+        path: "/",
+        replace: true,
       });
     }
-    next({
-      name: "Auth",
-    });
+  } else {
+    next();
   }
-}); */
+});
 
 export default router;
