b07d17b42263872343528180e8813202aafcf588
Felipe Tavares <felipelimatavares96@gmail.com>
2022-10-21 20:21:55 -0300

    Create component to represent documents

---
+++ src/components/chat/main/InputMessage/DocumentsComp/DocumentsComp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="DocumentsComp">
+    <FileDocumentOutline :size="65" class="DocumentIcon" />
+    <span>{{ name }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import FileDocumentOutline from "../../../../../../node_modules/vue3-material-design-icons-ts/dist/FileDocumentOutline.vue";
+
+export default defineComponent({
+  name: "DocumentsComp",
+  components: {
+    FileDocumentOutline,
+  },
+  props: {
+    name: String,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import "./DocumentsComp.scss";
+</style>
