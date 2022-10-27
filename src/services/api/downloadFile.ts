8062af9e89b7690b02fa058f7c70f84cf1f35023
Felipe Tavares <felipelimatavares96@gmail.com>
2022-10-26 22:52:22 -0300

    fix download service function

--- src/services/api/downloadFile.ts
+++ src/services/api/downloadFile.ts
@@ -1,18 +1,27 @@
-import { GetFileDownloadUrlData, GetFileDownloadUrlResponse } from "@/types";
-import { HttpClient } from "./HttpClient";
+import axios from "axios";
 
-const httpClient = HttpClient.getInstance();
-
-export async function getFileDownloadUrl(data: GetFileDownloadUrlData) {
+export async function downloadFile(
+  fileUrl: string,
+  fileName: string,
+  fileType: string
+): Promise<void> {
   try {
-    const response = await httpClient.client.post<GetFileDownloadUrlResponse>(
-      `/chat/files`,
-      data
-    );
+    const response = await axios.get(fileUrl, {
+      responseType: "arraybuffer",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: fileType,
+      },
+    });
 
-    if (response.status === 201) {
-      return response.data;
-    }
+    const url = window.URL.createObjectURL(
+      new Blob([response.data], { type: fileType })
+    );
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", fileName);
+    document.body.appendChild(link);
+    link.click();
   } catch (error) {
     console.log("Deu erro");
   }
